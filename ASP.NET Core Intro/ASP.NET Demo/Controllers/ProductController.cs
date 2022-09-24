de376be3874c90bc3fdc8ad99eabc6f49d64a69d
Иксан <iksan168@abv.bg>
2022-09-24 14:39:41 +0300

    Added ProductController, ViewModel and 2 views

---
+++ ASP.NET Core Intro/ASP.NET Demo/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using ASP.NET_Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Demo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IEnumerable<ProductViewModel> products =
+            new List<ProductViewModel>()
+            {
+                new ProductViewModel()
+                {
+                    Id = 1,
+                    Name = "Cheese",
+                    Price = 7.00
+                },
+                new ProductViewModel()
+                {
+                    Id = 2,
+                    Name = "Ham",
+                    Price = 5.50
+                },
+                 new ProductViewModel()
+                {
+                    Id = 3,
+                    Name = "Bread",
+                    Price = 1.50
+                }
+
+            };
+        public IActionResult Index()
+        {
+            return View();
+        }
+         public IActionResult All()
+        {
+            return View(products);
+        }
+
+    }
+}
---
+++ ASP.NET Core Intro/ASP.NET Demo/Models/ProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_Demo.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
