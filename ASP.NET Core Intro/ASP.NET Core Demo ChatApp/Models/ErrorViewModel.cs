3f6f778c72a499d54f2e5d0a234f7980da53968c
Иксан <iksan168@abv.bg>
2022-09-26 17:32:51 +0300

    Added dropdown menu

---
+++ ASP.NET Core Intro/ASP.NET Core Demo ChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Core_Demo_ChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
